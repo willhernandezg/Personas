02169b6060813123fa3e10b3d3facaa863804981
Will <willhernandezg@outlook.com>
2017-09-30 09:20:00 -0500

    Interfaces terminadas

---
+++ app/src/main/java/me/willhernandezg/personas/CrearPersonas.java
@@ -0,0 +1,13 @@
+package me.willhernandezg.personas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearPersonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_personas);
+    }
+}
